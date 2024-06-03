8b90d89fe727a82916afdf6e7b9e2e1b190b826f
broziedan <89726615+broziedan@users.noreply.github.com>
2024-06-03 11:32:47 +0100

    Removed one of the Services

--- pages/home.php
+++ pages/home.php
@@ -288,24 +288,7 @@
                 <img src="<?php echo $dot;?>assets/img/capital-multimedia-community.jpg" alt="" class="stack-back">
             </div>
           </div>
-        </div><!-- Features Item 
-
-        <div class="row gy-4 align-items-stretch justify-content-between features-item ">
-          <div class="col-lg-6 d-flex align-items-center features-img-bg" data-aos="zoom-out">
-            <img src="assets/img/features-light-3.jpg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-5 d-flex justify-content-center flex-column" data-aos="fade-up">
-            <h3>Sunt consequatur ad ut est nulla</h3>
-            <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
-            <ul>
-              <li><i class="bi bi-check"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
-              <li><i class="bi bi-check"></i><span> Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
-              <li><i class="bi bi-check"></i> <span>Facilis ut et voluptatem aperiam. Autem soluta ad fugiat</span>.</li>
-            </ul>
-            <a href="#" class="btn btn-get-started align-self-start">Get Started</a>
-          </div>
         </div><!-- Features Item -->
-
       </div>
 
     </section><!-- /Features Section -->
