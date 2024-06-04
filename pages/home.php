47f26b604fdffad53e1a80ec7ebe54b591d3a330
broziedan <89726615+broziedan@users.noreply.github.com>
2024-06-04 02:53:31 +0100

    Corrected email address Placeholder

--- pages/home.php
+++ pages/home.php
@@ -15,7 +15,7 @@
           </div>
           <div class="col-lg-5">
             <form action="#" class="sign-up-form d-flex" data-aos="fade-up" data-aos-delay="300">
-              <input type="text" class="form-control" placeholder="Enter email address">
+              <input type="text" class="form-control" placeholder="Enter your email address">
               <input type="submit" class="btn btn-primary" value="Sign up">
             </form>
           </div>
